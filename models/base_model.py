70f9ec036bee7fb67c572f65926e7994dfa4a849
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-05-14 22:38:09 +0300

    fixed id

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         '''
         intiating basemodel instance 
         '''
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
 

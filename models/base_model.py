9851ecea593e31d60cffe1a1fc8e518b69557230
SondosEssam <sondosessam713@gmail.com>
2024-05-20 00:23:10 +0300

    pystyle

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         '''
         intiating basemodel instance
         '''
-        if(len(kwargs) == 0):
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
